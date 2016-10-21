da0cb9e37c3c4a0a2ffce45f4100b9c7395fb938
Alex Couper <amcouper@gmail.com>
2016-10-21 14:52:10 +0000

    Use tox to test py27 and py35

--- component-builder/adi/test.sh
+++ component-builder/adi/test.sh
@@ -2,7 +2,8 @@
 set -e
 set -o pipefail
 
-REPORT_LOCATION=${REPORT_LOCATION:-$PROJECT_DIR}
+REPORT_LOCATION=${REPORT_LOCATION:-.}
 
-nosetests --with-xunit --xunit-file=$REPORT_LOCATION/nosetests.xml -s $PROJECT_DIR
+nosetests --with-xunit --xunit-file=$REPORT_LOCATION/nosetests.xml -s .
+
 flake8 src | tee $REPORT_LOCATION/flake8.txt
